84b9a84a59262a9eb93b1ac8a2dfb16f83e6cafb
Jorge Sanchez <jorgesinsilla@gmail.com>
2023-04-04 20:41:33 +0100

    fix cart & navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 
 export function Navbar() {
   return (
-    <NavbarBs className="bg-white shadow-sm mb-3">
+    <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
       <Container>
         <Nav className="me-auto">
           <Nav.Link to={"/"} as={NavLink}>
@@ -17,15 +17,29 @@ export function Navbar() {
           </Nav.Link>
         </Nav>
         <Button
-          style={{ width: "3rem", height: "3rem" }}
+          style={{ width: "3rem", height: "3rem", position: "relative" }}
           variant="outline-primary"
           className="rounded-circle"
         >
           <img
             src="/images/cart-icon.png"
             alt="cart-icon"
-            style={{ width: "1.5rem", height: "1.5rem"}}
+            style={{ width: "1.5rem", height: "1.5rem" }}
           />
+          <div
+            className="rounded-circle bg-danger d-flex justify-content-center align-items-center"
+            style={{
+              color: "white",
+              width: "1.5rem",
+              height: "1.5rem",
+              position: "absolute",
+              bottom: 0,
+              right: 0,
+              transform: "translate(25%, 25%)",
+            }}
+          >
+            3
+          </div>
         </Button>
       </Container>
     </NavbarBs>
